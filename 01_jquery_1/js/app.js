df78488240fb7820c27cfe7262197d15bd184f68
masteren <1447667558@qq.com>
2026-04-22 10:58:17 +0900

    jQuery 基础 / jQuery 基礎 / jQuery basics

--- 01_jquery_1/js/app.js
+++ 01_jquery_1/js/app.js
@@ -1,8 +1,11 @@
 // バニラJSで、DOM読み込み後にアラート表示
-// document.addEventListener("DOMContentLoaded", function () {
-//     alert("バニラJS")
-// })
+document.addEventListener("DOMContentLoaded", function () {
+    alert("バニラJS")
+})
 
 // TODO: jQueryで、DOM読み込み後にアラート表示
-
+$(function () {
+    alert("DOMが読み込まれました！")
+});
 // TODO:DOM読み込み前の処理
+alert("始まります！")
